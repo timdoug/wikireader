b9512a8b19a64b9dd6c36e342a753bcf16228884
Tim Douglas <me@timdoug.com>
2026-08-30 21:24:25 -0400

    tests: track C33 GAS dump expectations

---
+++ host-tools/toolchain-c33/binutils/files/gas/testsuite/gas/c33/pe-added.d
@@ -0,0 +1,13 @@
+#as: -mc33pe
+#objdump: -d
+#name: C33 PE retains documented instructions
+
+.*: +file format elf32-c33
+
+Disassembly of section .text:
+
+0+ <.text>:
+ +0:\t.*jpr +%r1.*
+ +2:\t.*swaph +%r0,%r1.*
+ +4:\t.*adc +%r0,%r1.*
+ +6:\t.*sbc +%r0,%r1.*
