6f9df37ee66f926e5d81d6366aa67912daacd0a2
Tim Douglas <me@timdoug.com>
2026-09-04 12:16:41 -0700

    wiki: clear only the draw-buffer rows an article used
    init_render_article cleared the whole 3.8 MiB off-screen buffer for every
    article, about 170 ms on the device.  Track the rows each writer touches and
    clear just those.

--- wiki/lcd_buf_draw.c
+++ wiki/lcd_buf_draw.c
@@ -295,6 +295,32 @@ void load_all_fonts()
 }
 
 
+/*
+ * Rows of screen_buf that may hold ink since it was last cleared.  Clearing
+ * the whole 3.8 MiB buffer for every article costs about 170 ms on the
+ * device; only the rows the previous article actually reached need it.
+ * Every writer into screen_buf records the rows it touches.
+ */
+static long lcd_draw_buf_rows_used;
+
+static void lcd_draw_buf_touch(long end_y)
+{
+	if (end_y > LCD_BUF_HEIGHT_PIXELS)
+		end_y = LCD_BUF_HEIGHT_PIXELS;
+	if (end_y > lcd_draw_buf_rows_used)
+		lcd_draw_buf_rows_used = end_y;
+}
+
+static void lcd_draw_buf_clear(void)
+{
+	if (!lcd_draw_buf.screen_buf)
+		return;
+	if (lcd_draw_buf_rows_used > 0)
+		memset(lcd_draw_buf.screen_buf, 0,
+		       (size_t)lcd_draw_buf_rows_used * LCD_BUF_WIDTH_BYTES);
+	lcd_draw_buf_rows_used = 0;
+}
+
 void init_lcd_draw_buf()
 {
 	int i,framebuffersize;
@@ -305,6 +331,7 @@ void init_lcd_draw_buf()
 		lcd_default_framebuffer = lcd_get_framebuffer();
 		framebuffer_copy = (unsigned char*)memory_allocate(framebuffersize, "bufdraw1");
 		lcd_draw_buf.screen_buf = (unsigned char *)memory_allocate(LCD_BUF_WIDTH_BYTES * LCD_BUF_HEIGHT_PIXELS, "bufdraw2");
+		lcd_draw_buf_rows_used = LCD_BUF_HEIGHT_PIXELS; /* fresh heap memory is not zeroed */
 		if (!framebuffer_copy || !lcd_draw_buf.screen_buf)
 			fatal_error("lcd_draw_buf allocation error");
 
@@ -358,8 +385,7 @@ void init_lcd_draw_buf()
 	lcd_draw_buf.y_adjustment = 0;
 	nArticleRenderedLines = 0;
 
-	if (lcd_draw_buf.screen_buf)
-		memset(lcd_draw_buf.screen_buf, 0, LCD_BUF_WIDTH_BYTES * LCD_BUF_HEIGHT_PIXELS);
+	lcd_draw_buf_clear();
 }
 
 void draw_string(const unsigned char *s)
@@ -396,6 +422,7 @@ void draw_article_positioner(int y_pos)
 			guilib_buffer_set_pixel(lcd_draw_buf.screen_buf, 0, last_positioner_y + 1);
 			guilib_buffer_set_pixel(lcd_draw_buf.screen_buf, 1, last_positioner_y + 1);
 			guilib_buffer_set_pixel(lcd_draw_buf.screen_buf, 0, last_positioner_y + 2);
+			lcd_draw_buf_touch(last_positioner_y + 3);
 		}
 	}
 }
@@ -660,6 +687,7 @@ void buf_draw_UTF8_str(const unsigned char **pUTF8)
 					nImageY++;
 				}
 			}
+			lcd_draw_buf_touch(nImageY);
 			lcd_draw_buf.current_x += nWidth;
 			break;
 		default:
@@ -817,6 +845,7 @@ void buf_draw_horizontal_line(unsigned long start_x, unsigned long end_x)
 	{
 		guilib_buffer_set_pixel(lcd_draw_buf.screen_buf,i, h_line_y);
 	}
+	lcd_draw_buf_touch(h_line_y + 1);
 
 }
 
@@ -829,6 +858,7 @@ void buf_draw_vertical_line(unsigned long start_y, unsigned long end_y)
 	{
 		idx_in_byte = 7 - ((lcd_draw_buf.current_x + LCD_LEFT_MARGIN + lcd_draw_buf.x_adjustment) & 0x07);
 		p = lcd_draw_buf.screen_buf + start_y * LCD_BUF_WIDTH_BYTES + ((lcd_draw_buf.current_x + LCD_LEFT_MARGIN + lcd_draw_buf.x_adjustment)>> 3);
+		lcd_draw_buf_touch((long)end_y + 1);
 		while (start_y <= end_y)
 		{
 			*p |= 1 << idx_in_byte;
@@ -889,6 +919,7 @@ void buf_draw_char(ucs4_t u)
 		y_base = lcd_draw_buf.current_y + lcd_draw_buf.y_adjustment;
 		x_offset = 0;
 		y_offset = lcd_draw_buf.line_height - (lcd_draw_buf.pPcfFont->Fmetrics.descent + Cmetrics.ascent);
+		lcd_draw_buf_touch(y_base + y_offset + Cmetrics.height + 1);
 
 		for (i = 0; i < bytes_to_process; i++)
 		{
@@ -1150,8 +1181,7 @@ void init_render_article(long init_y_pos)
 	//if(lcd_draw_buf.current_y>0)
 	//  memset(lcd_draw_buf.screen_buf,0,lcd_draw_buf.current_y*LCD_BUFFER_WIDTH/8);
 	highlight_reset(-1, -1, false);
-	if (lcd_draw_buf.screen_buf)
-		memset(lcd_draw_buf.screen_buf, 0, LCD_BUF_WIDTH_BYTES * LCD_BUF_HEIGHT_PIXELS);
+	lcd_draw_buf_clear();
 
 	article_buf_pointer = NULL;
 	lcd_draw_buf.current_x = 0;
@@ -1189,6 +1219,7 @@ void render_wikipedia_license_text(void)
 	if (draw_lines < 0)
 		draw_lines = 0;
 	memcpy(&lcd_draw_buf.screen_buf[lcd_draw_buf.current_y * LCD_BUF_WIDTH_BYTES], license_draw->buf, draw_lines * LCD_BUF_WIDTH_BYTES);
+	lcd_draw_buf_touch(lcd_draw_buf.current_y + draw_lines);
 	for (i = 0; i < license_draw->link_count && article_link_count < MAX_ARTICLE_LINKS; i++)
 	{
 		start_x = license_draw->links[i].start_xy & 0xFF;
@@ -1512,6 +1543,7 @@ int render_search_result_with_pcf(void)
 		offset_next = result_list_offset_next();
 		init_render_article(0);
 		memcpy(lcd_draw_buf.screen_buf, lcd_get_framebuffer(), framebuffer_size()); // copy from the LCD frame buffer (for the first page)
+		lcd_draw_buf_touch(LCD_HEIGHT);
 		display_first_page = 1;
 		lcd_draw_buf.pPcfFont = &pcfFonts[SEARCH_LIST_FONT_IDX - 1];
 		lcd_draw_buf.line_height = RESULT_HEIGHT;
@@ -1711,6 +1743,7 @@ void draw_icon(const unsigned char *pStr)
 	int i, j;
 
 	get_external_str_pixel_rectangle(pStr, SUBTITLE_FONT_IDX, &start_x, &start_y, &end_x, &end_y);
+	lcd_draw_buf_touch(lcd_draw_buf.current_y + LANGUAGE_LINK_HEIGHT + 16);
 	for (i = 0; i < LANGUAGE_LINK_HEIGHT; i++)
 	{
 		if (i == 1 || i == LANGUAGE_LINK_HEIGHT - 2)
@@ -3123,6 +3156,7 @@ void draw_highlight_area(int start_line, int end_line, int start_x, int end_x,
 			else
 				ye = pArticleRenderInfo[i].end_y;
 			guilib_buffer_invert_area(lcd_draw_buf.screen_buf, xs, ys, xe, ye);
+			lcd_draw_buf_touch((long)ye + 1);
 		}
 	}
 
@@ -3173,6 +3207,7 @@ void draw_highlight_area(int start_line, int end_line, int start_x, int end_x,
 			else
 				ye = pArticleRenderInfo[i].end_y;
 			guilib_buffer_invert_area(lcd_draw_buf.screen_buf, xs, ys, xe, ye);
+			lcd_draw_buf_touch((long)ye + 1);
 
 			if (i == iLineStart)
 			{
